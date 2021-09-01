284624c71b23d9a633e5952be2aa8fe8db6270cd
Salatiel Souza <sal.souzasa@gmail.com>
2021-09-01 20:48:23 -0300

    fix

--- src/commons/abstrata.py
+++ src/commons/abstrata.py
@@ -1,6 +1,7 @@
-from abc ABC,
+from abc import ABC,abstractmethod
 
 class Abstrata(ABC):
+
     def __init__(self, fr_path_al, fr_path_disc, to_path):
         self.fr_path_al = fr_path_al
         self.fr_path_disc = fr_path_disc
