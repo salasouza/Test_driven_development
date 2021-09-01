2dcc7388e5a0a177f7159ea28a1705ec42bfe3e7
Salatiel Souza <sal.souzasa@gmail.com>
2021-09-01 20:46:24 -0300

    fix

--- src/commons/abstrata.py
+++ src/commons/abstrata.py
@@ -1,10 +1,11 @@
-class Abstrata:
+from abc ABC,
+
+class Abstrata(ABC):
     def __init__(self, fr_path_al, fr_path_disc, to_path):
         self.fr_path_al = fr_path_al
         self.fr_path_disc = fr_path_disc
         self.to_path = to_path
 
+    @abstractmethod # não vai implementar esse método, vai ser implementado por quem herdar esse método.
     def processa():
         return 1
-
-        
