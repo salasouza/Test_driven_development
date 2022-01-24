c6c4fa6e73aab9712b3735bba72525f1ae5ac104
Salatiel Souza <sal.souzasa@gmail.com>
2022-01-23 21:45:27 -0300

    add new files

--- src/jobs/to_trusted.py
+++ src/jobs/to_trusted.py
@@ -1,13 +1,10 @@
 from pyspark.sql import SparkSession
-from src.commons.Abstrata import Abstrata
+from src.commons.abstrata import Abstrata
 
 
 class ToTrusted(Abstrata):
     def __init__(self, fr_path_al,fr_path_disc,to_path):
         super(ToTrusted, self).__init__()
-        # self.fr_path_al = fr_path_al
-        # self.fr_path_disc = fr_path_disc
-        # self.to_path = to_path
 
     def processa(self):
         spark = SparkSession.builder.getOrCreate()
@@ -22,15 +19,4 @@ class ToTrusted(Abstrata):
         df_output.show()
 
         df_output.write.format('delta').mode('overwrite').save(self.to_path)
-        # # # df.write(self.to_path)
-        # print('deu certo')
         print('Test Sucess')
-        #return 1
-
-## CSV Aluno, CSV de disciplina
-#
-# def function:
-#       for file in path:
-#             a = a[i]
-#               b = [i]
-#              a.merge(b)
